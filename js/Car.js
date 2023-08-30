917f70a398b5ceeed5e9d4f1592355b04c6490d3
akhnikh <114444837+akhnikh@users.noreply.github.com>
2023-08-30 18:19:53 -0500

    Add files via upload

---
+++ js/Car.js
@@ -0,0 +1,33 @@
+AFRAME.registerComponent("car-model", {
+  schema: {
+    modelRef: { type: "string", default: "../assets/car/scene.gltf" },
+    clickCounter: {type: 'number', default: 0}
+  },
+  init: function() {
+    // Do something when component first attached.
+    this.el.setAttribute("gltf-model", this.data.modelRef);
+    const position = { x: 0, y: 50, z: 80 };
+    const rotation = { x: 0, y: -100, z: 0 };
+    this.el.setAttribute("position", position);
+    this.el.setAttribute("rotation", rotation);
+  },
+  update: function() {
+    window.addEventListener('click', (e)=>{
+      this.data.clickCounter = this.data.clickCounter + 1;
+      if (this.data.clickCounter === 1){
+        const rotation = {x: 0, y: 20, z: 0};
+        this.el.setAttribute('rotation', rotation);
+      } else if (this.data.clickCounter ===  2) {
+        const rotation = {x: 0, y: 100, z: 0};
+      } else if (this.data.clickCounter ===  3) {
+        const rotation = {x: 0, y: -200, z: 0};
+      } else if (this.data.clickCounter ===  4) {
+        const rotation = {x: 0, y: -100, z: 0};
+      } else if (this.data.clickCounter ===  5) {
+        const rotation = {x: 0, y: -20, z: 0};
+        var camElement = document.querySelector('#camera')
+        camElement.setAttribute('position', {x: 0, y: 195, z: 450})
+      } 
+    })
+  }
+});
